210988de2a50eb713e590c8e14af9ccb06180ff9
Vladimir Vovk <vladimir@vovk.in>
2019-05-19 18:49:30 +0300

    fix: server static by express

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 /* Serve static */
-app.use(express.static(path.join(global.appRoot, 'assets')))
+app.use(express.static('assets'))
 
 router(app)
 
